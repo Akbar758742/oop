4b3f241e07b6dd45f2027e5a2bca2aa2f67192fe
akbar hossen <mdakbar758742@gmail.com>
2023-12-18 22:59:33 -0800

    1st commit

---
+++ h.js
@@ -0,0 +1,6 @@
+function addToCart() {
+    // Logic to add the product to the cart
+    alert('Product added to cart!');
+    // You'd likely have more sophisticated logic to add items to a shopping cart system.
+  }
+  
